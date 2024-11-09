2e4e815f6ed11bf612327d043df865f6ba13eebe
Mthoko1196 <mthokozisi1196@gmail.com>
2024-11-09 13:38:11 +0200

    deploy 6

--- Modules/ec2/outputs.tf
+++ Modules/ec2/outputs.tf
@@ -8,10 +8,10 @@
 #   value       = aws_instance.web.public_ip
 # }
 
-output "security_group_id" {
-  description = "Security group ID for the EC2 instance"
-  value       = aws_security_group.web_sg.id
-}
+# output "security_group_id" {
+#   description = "Security group ID for the EC2 instance"
+#   value       = aws_security_group.web_sg.id
+# }
 
 output "instance_id" {
   value = aws_instance.web.id
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
+# output "ec2_public_ip" {
+#   value = module.ec2.public_ip
+# }
 
-output "ecr_repository_url" {
-  value = module.ecr.repository_url
-}
+# output "ecr_repository_url" {
+#   value = module.ecr.repository_url
+# }
 
-output "rds_endpoint" {
-  value = module.rds.rds_endpoint
-}
+# output "rds_endpoint" {
+#   value = module.rds.rds_endpoint
+# }
